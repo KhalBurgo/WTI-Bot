19427763b14ae0d75db5c3b9dce38b53fea324c5
KhalBurgo <134977506+KhalBurgo@users.noreply.github.com>
2025-05-29 13:30:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 import os
-
 from keep_alive import keep_alive
+from config import GUILD # ← OK ORA
 
 intents = discord.Intents.default()
 intents.message_content = True
